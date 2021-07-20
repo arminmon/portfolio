8ba92ee3a385cdadcf65a51e9cb4a4da08466121
Armin <armin.monirzadeh@gmail.com>
2021-07-20 14:37:42 +1000

    expand each skill into a dialog

--- components/resume/SkillsetsGrid.vue
+++ components/resume/SkillsetsGrid.vue
@@ -18,7 +18,13 @@
 				:key="`${skillset.slug}-${item.title}`"
 				cols="auto"
 			>
-				<v-chip pill :color="item.color" :dark="item.dark" :light="item.light">
+				<v-chip
+					pill
+					:color="item.color"
+					:dark="item.dark"
+					:light="item.light"
+					@click="openDialog(item)"
+				>
 					<v-avatar v-if="item.icon" left color="rgba(255,255,255,.2)">
 						<v-icon size="18" class="rounded-0">
 							{{ item.icon }}
@@ -28,6 +34,55 @@
 				</v-chip>
 			</v-col>
 		</v-row>
+		<v-dialog
+			v-if="skill"
+			v-model="dialog"
+			max-width="320px"
+			:overlay-color="skill.color"
+			overlay-opacity=".2"
+		>
+			<v-card
+				:color="skill.color"
+				:dark="skill.dark"
+				:light="skill.light"
+				:style="{ position: 'relative', overflow: 'hidden' }"
+			>
+				<v-icon
+					v-if="skill.icon"
+					size="200"
+					:style="{
+						position: 'absolute',
+						top: '-15%',
+						right: '-15%',
+						opacity: '.1',
+					}"
+				>
+					{{ skill.icon }}
+				</v-icon>
+				<v-card-title>{{ skill.title }}</v-card-title>
+				<v-card-subtitle v-if="skill.subtitle">
+					{{ skill.subtitle }}
+				</v-card-subtitle>
+				<v-card-text v-if="skill.notes">
+					<ul>
+						<li
+							v-for="(note, index) in skill.notes"
+							:key="`${skill.title}-note-${index}`"
+						>
+							{{ note }}
+						</li>
+					</ul>
+				</v-card-text>
+				<v-card-actions>
+					<v-btn text plain @click="closeDialog">Close</v-btn>
+					<v-spacer />
+					<v-btn text link target="_blank" :href="skill.link">
+						<v-icon left>$icon.mdiLinkVariant</v-icon>
+						Learn more
+					</v-btn>
+				</v-card-actions>
+			</v-card>
+		</v-dialog>
 	</v-container>
 </template>
 
@@ -37,6 +92,19 @@ export default {
 		skillsets: {
 			type: Array,
 			required: true,
+		},
+	},
+	data: () => ({
+		dialog: false,
+		skill: null,
+	}),
+	methods: {
+		openDialog(skill) {
+			this.skill = skill
+			this.dialog = true
+		},
+		closeDialog() {
+			this.dialog = false
 		},
 	},
 }
