051d3bdf9dd3f5511013df906d0800cf28ebff33
Armin <armin.monirzadeh@gmail.com>
2021-08-03 16:08:31 +1000

    separate project tools

--- locales/fa.ts
+++ locales/fa.ts
@@ -46,6 +46,7 @@ const messages = {
 	'Frontend Development': 'توسعه فرانت‌اند',
 	'Backend Development': 'توسعه بک‌اند',
 	'Development Tools': 'ابزار توسعه',
+	'Project Tools': 'ابزار پروژه',
 	'Principles': 'اصول',
 	'Graphic Design': 'طراحی گرافیک',
 	'Operating Systems': 'سیستم عامل',
