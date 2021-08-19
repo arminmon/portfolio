d55980f679ea56ecf68424680e6285befd2ded60
Armin <armin.monirzadeh@gmail.com>
2021-08-19 23:28:09 +1000

    add avatar

--- pages/index.vue
+++ pages/index.vue
@@ -73,6 +73,11 @@
 							{{ $vuetify.breakpoint.smAndDown ? $t('Hi') : $t('Hello') }}
 							👋,&nbsp;
 							<span class="text-nowrap">{{ $t("I'm Armin") }}.</span>
+							<v-avatar size="64" class="ms-2 ms-md-4">
+								<v-img
+									src="https://secure.gravatar.com/avatar/14b8d65cfe37e16e124d3d37106e3cab?s=300"
+								/>
+							</v-avatar>
 						</v-card-title>
 					</v-card>
 				</v-col>
