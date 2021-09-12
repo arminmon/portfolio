7f9f80f027dcab3dd11394d4b4a00fc6644c8022
Armin <armin.monirzadeh@gmail.com>
2021-09-12 18:01:32 +1000

    decrease main container max-width to 960px

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
 	<v-sheet class="pb-12">
-		<v-container :style="{ maxWidth: '1000px' }">
+		<v-container :style="{ maxWidth: '960px' }">
 			<v-row>
 				<v-col cols="12" class="pt-0">
 					<v-card
