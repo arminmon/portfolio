575842511882806b43a04a26b043239ee6f4a7f1
Armin <armin.monirzadeh@gmail.com>
2021-10-04 11:00:28 +1100

    include sitemap in footer

--- components/App/Footer.vue
+++ components/App/Footer.vue
@@ -24,56 +24,126 @@
 					</v-col>
 				</template>
 			</v-row>
+			<v-divider class="my-12" />
+			<v-row justify="space-around">
+				<v-col cols="12" md="4">
+					<v-subheader class="text-caption">
+						{{ $t('on this page') }}
+					</v-subheader>
+					<v-list nav dense color="transparent">
+						<v-list-item
+							v-for="(navLink, index) in navLinks"
+							:key="`footer-navLink-${index}`"
+							:to="navLink.to"
+						>
+							<v-list-item-icon>
+								<v-icon>{{ navLink.icon }}</v-icon>
+							</v-list-item-icon>
+							<v-list-item-content>
+								<v-list-item-title>{{ navLink.title }}</v-list-item-title>
+							</v-list-item-content>
+						</v-list-item>
+					</v-list>
+				</v-col>
+				<v-col cols="12" md="4">
+					<v-subheader class="text-caption">
+						{{ $t('externals') }}
+					</v-subheader>
+					<v-list nav dense color="transparent">
+						<v-list-item
+							v-for="(socialLink, index) in socialLinks"
+							:key="`footer-socialLink-${index}`"
+							target="_blank"
+							:href="socialLink.href"
+						>
+							<v-list-item-icon>
+								<v-icon :color="socialLink.color">{{ socialLink.icon }}</v-icon>
+							</v-list-item-icon>
+							<v-list-item-content>
+								<v-list-item-title>{{ socialLink.title }}</v-list-item-title>
+							</v-list-item-content>
+							<v-list-item-icon class="align-center">
+								<v-icon small> $icon.external_link </v-icon>
+							</v-list-item-icon>
+						</v-list-item>
+					</v-list>
+				</v-col>
+				<v-col cols="12" md="4">
+					<v-subheader class="text-caption">
+						{{ $t('get in touch') }}
+					</v-subheader>
+					<v-text-field v-model="email" :label="$t('E-mail')" readonly>
+						<template #append>
+							<v-tooltip
+								top
+								:color="
+									copyEmailToClipboardLabelText === 'Copied!'
+										? 'success'
+										: undefined
+								"
+							>
+								<template #activator="{ on, attrs }">
+									<v-btn
+										v-bind="attrs"
+										icon
+										v-on="on"
+										@click="copyEmailToClipboard"
+									>
+										<v-icon>$icon.copy</v-icon>
+									</v-btn>
+								</template>
+								<span>{{ $t(copyEmailToClipboardLabelText) }}</span>
+							</v-tooltip>
+						</template>
+					</v-text-field>
+				</v-col>
+			</v-row>
 		</v-container>
 	</v-footer>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
-import { BuildingBlock } from '~/types/models'
+import buildingBlocks from '~/content/app/building-blocks.json'
+import socialLinks from '~/content/socials.json'
+import type { NavLink } from '~/types/elements'
+import type { BuildingBlock, SocialLink } from '~/types/models'
 
 @Component
 export default class AppFooter extends Vue {
-	blocks: BuildingBlock[] = [
+	blocks: BuildingBlock[] = buildingBlocks
+	email: string = socialLinks.find((item) => item.slug === 'email')!.title
+	copyEmailToClipboardLabelText: string = 'Copy '
+	copyToClipboardLabelText: string = 'Copy'
+	socialLinks: SocialLink[] = socialLinks.filter(
+		(item) => item.slug !== 'email'
+	)
+
+	navLinks: NavLink[] = [
 		{
-			title: 'Nuxt',
-			icon: '$icon.nuxt',
+			title: 'Employment History',
+			icon: '$icon.domain',
+			to: '/#employment_history',
 		},
 		{
-			title: 'Vue',
-			icon: '$icon.vue_js',
+			title: 'Education History',
+			icon: '$icon.school',
+			to: '/#education_history',
 		},
 		{
-			title: 'Vuetify',
-			icon: '$icon.vuetify',
+			title: 'Professional Memberships',
+			icon: '$icon.handshake',
+			to: '/#membership_history',
 		},
 		{
-			title: 'TypeScript',
-			icon: '$icon.typescript',
+			title: 'Technical Skillsets',
+			icon: '$icon.pencil_ruler',
+			to: '/#technical_skillsets',
 		},
 		{
-			title: 'Google Material Design',
-			icon: '$icon.material_design',
-		},
-		{
-			title: 'Material Design Icons',
-			icon: '$icon.material_design_icons',
-		},
-		{
-			title: 'Simple Icons',
-			icon: '$icon.simple_icons',
-		},
-		{
-			title: 'Noun Project',
-			icon: '$icon.noun_project',
-		},
-		{
-			title: 'Vercel',
-			icon: '$icon.vercel',
-		},
-		{
-			title: 'Some theine',
-			icon: '$icon.tea',
+			title: 'Soft Skills',
+			icon: '$icon.human_greeting',
+			to: '/#soft_skills',
 		},
 	]
 
@@ -87,6 +157,14 @@ export default class AppFooter extends Vue {
 		)
 
 		return `This website was built using ${blocksList}`
+	}
+
+	copyEmailToClipboard() {
+		navigator.clipboard.writeText(this.email)
+		this.copyEmailToClipboardLabelText = 'Copied!'
+		setTimeout(() => {
+			this.copyEmailToClipboardLabelText = this.copyToClipboardLabelText
+		}, 1000)
 	}
 }
 </script>
