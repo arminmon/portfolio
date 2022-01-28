a8323fab12c9b61220f42c6b1b9df65fadce97ce
Armin <armin.monirzadeh@gmail.com>
2022-01-29 10:05:59 +1100

    move landing header banner into a separate component

---
+++ components/Landing/HeaderBanner.vue
@@ -0,0 +1,31 @@
+<template>
+	<v-card tag="header" role="banner" elevation="18" class="rounded-t-0">
+		<v-card-title
+			:class="[
+				'text-h4',
+				'text-center',
+				'justify-center',
+				'pa-6',
+				'pa-md-8',
+				'pa-lg-10',
+				'pa-xl-12',
+			]"
+		>
+			{{ $vuetify.breakpoint.smAndDown ? $t('Hi') : $t('Hello') }}
+			👋,&nbsp;
+			<span class="text-nowrap">{{ $t("I'm Armin") }}.</span>
+			<v-avatar size="64" class="ms-2 ms-md-4">
+				<v-img
+					src="https://secure.gravatar.com/avatar/14b8d65cfe37e16e124d3d37106e3cab?s=300"
+				/>
+			</v-avatar>
+		</v-card-title>
+	</v-card>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class LandingHeaderBanner extends Vue {}
+</script>
