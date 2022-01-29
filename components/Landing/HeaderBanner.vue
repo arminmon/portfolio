1f1ced0cbfcc30f9ac801d8dd20165cfa09e6170
Armin Monirzadeh <arminmon@outlook.com>
2022-01-29 13:39:57 +1100

    Improve/header banner (#68)
    * yarn reinstall
    
    * move landing header banner into a separate component
    
    * add waving animation to the hand emoji
    
    * add Memoji images
    
    * define theme-color meta
    
    * redo header banner

---
+++ components/Landing/HeaderBanner.vue
@@ -0,0 +1,38 @@
+<template>
+	<v-card tag="header" role="banner" elevation="18" class="rounded-t-0">
+		<v-row no-gutters align="center" justify="center">
+			<v-col
+				cols="12"
+				md="auto"
+				:class="[
+					'text-h4',
+					'text-center',
+					'justify-center',
+					'pa-6',
+					'pa-md-8',
+					'pa-lg-10',
+					'pa-xl-12',
+				]"
+			>
+				{{ $vuetify.breakpoint.smAndDown ? $t('Hi') : $t('Hello') }},
+				<span class="text-nowrap">{{ $t("I'm Armin") }}.</span>
+				<div class="text-caption">{{ $t('A Software Engineer') }}</div>
+			</v-col>
+			<v-col cols="4" md="2" xl="1" class="pt-0 pt-md-6" align-self="end">
+				<v-img
+					src="/images/memoji/wave.png"
+					aspect-ratio="1"
+					position="bottom"
+					contain
+				/>
+			</v-col>
+		</v-row>
+	</v-card>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class LandingHeaderBanner extends Vue {}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,28 +3,7 @@
 		<v-container :style="{ maxWidth: '960px' }">
 			<v-row>
 				<v-col cols="12" class="pt-0">
-					<v-card tag="header" role="banner" elevation="18" class="rounded-t-0">
-						<v-card-title
-							:class="[
-								'text-h4',
-								'text-center',
-								'justify-center',
-								'pa-6',
-								'pa-md-8',
-								'pa-lg-10',
-								'pa-xl-12',
-							]"
-						>
-							{{ $vuetify.breakpoint.smAndDown ? $t('Hi') : $t('Hello') }}
-							👋,&nbsp;
-							<span class="text-nowrap">{{ $t("I'm Armin") }}.</span>
-							<v-avatar size="64" class="ms-2 ms-md-4">
-								<v-img
-									src="https://secure.gravatar.com/avatar/14b8d65cfe37e16e124d3d37106e3cab?s=300"
-								/>
-							</v-avatar>
-						</v-card-title>
-					</v-card>
+					<landing-header-banner />
 				</v-col>
 				<v-col cols="12">
 					<resume-social-links :items="socialLinks" />
