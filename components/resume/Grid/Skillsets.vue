57987d31e3670083a8ca49cbdef39baa80d56bb6
Armin <armin.monirzadeh@gmail.com>
2022-02-15 19:34:00 +1100

    use skill chip as direct dialog activator to bind attrs

--- components/resume/Grid/Skillsets.vue
+++ components/resume/Grid/Skillsets.vue
@@ -1,93 +1,175 @@
 <template>
-	<client-only>
-		<v-container>
-			<v-toolbar flat color="transparent" class="align-items-center">
-				<v-toolbar-title>
-					<h2
-						:class="[
-							'text-subtitle-1',
-							'text-md-h6',
-							'text-uppercase',
-							'text-wrap',
-							'primary--text',
-						]"
-					>
-						{{ $t('Technical Skillsets') }}
-					</h2>
-				</v-toolbar-title>
-				<v-spacer />
-				<span aria-hidden="true" class="text-caption font-italic">
-					<v-badge inline dot bordered color="primary" />
-					{{ $t('is preferred.') }}
-				</span>
-			</v-toolbar>
-			<v-toolbar dense flat color="transparent">
-				<v-text-field
-					v-model="search"
-					:placeholder="$t('Search for a skill')"
-					dense
-					solo
-					hide-details
-					clearable
-				/>
-			</v-toolbar>
-			<v-data-iterator
-				:items="items"
-				item-key="slug"
-				group-by="group"
-				:search="search"
-				:custom-filter="searchItems"
-				disable-pagination
-				hide-default-footer
-			>
-				<template #default="{ groupedItems }">
-					<v-row
-						v-for="group in groupedItems.sort(
-							(a, b) => groupOrders[a.name] - groupOrders[b.name]
-						)"
-						:key="group.name"
-					>
-						<v-col cols="12" class="pb-0">
-							<v-subheader>
-								<h3 class="text-subtitle-2">{{ $t(group.name) }}</h3>
-							</v-subheader>
-						</v-col>
-						<v-col v-for="item in group.items" :key="item.slug" cols="auto">
-							<v-badge :value="item.isPreferred" dot overlap bordered>
-								<v-chip
-									pill
-									:color="item.color"
-									:dark="!$colour.isReadableInBlack(item.color)"
-									:light="$colour.isReadableInBlack(item.color)"
-									@click="openInDialog(item)"
+	<v-container>
+		<v-toolbar flat color="transparent" class="align-items-center">
+			<v-toolbar-title>
+				<h2
+					:class="[
+						'text-subtitle-1',
+						'text-md-h6',
+						'text-uppercase',
+						'text-wrap',
+						'primary--text',
+					]"
+				>
+					{{ $t('Technical Skillsets') }}
+				</h2>
+			</v-toolbar-title>
+			<v-spacer />
+			<span aria-hidden="true" class="text-caption font-italic">
+				<v-badge inline dot bordered color="primary" />
+				{{ $t('is preferred.') }}
+			</span>
+		</v-toolbar>
+		<v-toolbar dense flat color="transparent">
+			<v-text-field
+				v-model="search"
+				:placeholder="$t('Search for a skill')"
+				dense
+				solo
+				hide-details
+				clearable
+			/>
+		</v-toolbar>
+		<v-data-iterator
+			:items="items"
+			item-key="slug"
+			group-by="group"
+			:search="search"
+			:custom-filter="searchItems"
+			disable-pagination
+			hide-default-footer
+		>
+			<template #default="{ groupedItems }">
+				<v-row
+					v-for="group in groupedItems.sort(
+						(a, b) => groupOrders[a.name] - groupOrders[b.name]
+					)"
+					:key="group.name"
+				>
+					<v-col cols="12" class="pb-0">
+						<v-subheader>
+							<h3 class="text-subtitle-2">{{ $t(group.name) }}</h3>
+						</v-subheader>
+					</v-col>
+					<v-col v-for="item in group.items" :key="item.slug" cols="auto">
+						<v-dialog
+							v-model="item.dialogIsShown"
+							max-width="320px"
+							width="300px"
+							:dark="!$colour.isReadableInBlack(item.color)"
+							:light="$colour.isReadableInBlack(item.color)"
+							:overlay-color="
+								$colour.setLightness(item.color, item.light ? 0.15 : 0.85)
+							"
+							overlay-opacity=".5"
+							aria-labelledby="test"
+						>
+							<template #activator="{ on, attrs }">
+								<v-badge :value="item.isPreferred" dot overlap bordered>
+									<v-chip
+										v-bind="attrs"
+										pill
+										:color="item.color"
+										:dark="!$colour.isReadableInBlack(item.color)"
+										:light="$colour.isReadableInBlack(item.color)"
+										v-on="on"
+									>
+										<v-avatar v-if="item.icon" left>
+											<v-icon class="rounded-0 pa-1">
+												{{ item.icon }}
+											</v-icon>
+										</v-avatar>
+										<span>{{ item.title }}</span>
+									</v-chip>
+								</v-badge>
+							</template>
+							<v-card
+								:color="item.color"
+								:dark="!$colour.isReadableInBlack(item.color)"
+								:light="$colour.isReadableInBlack(item.color)"
+								:style="{ position: 'relative', overflow: 'hidden' }"
+							>
+								<v-icon
+									v-if="item.icon"
+									size="200"
+									:style="{
+										position: 'absolute',
+										left: !$vuetify.rtl ? 'unset' : '-8px',
+										right: $vuetify.rtl ? 'unset' : '-8px',
+										top: '-8px',
+										opacity: '.075',
+									}"
 								>
-									<v-avatar v-if="item.icon" left>
-										<v-icon class="rounded-0 pa-1">
-											{{ item.icon }}
-										</v-icon>
-									</v-avatar>
-									<span>{{ item.title }}</span>
-								</v-chip>
-							</v-badge>
-						</v-col>
-					</v-row>
-				</template>
-				<template #no-results>
-					<v-row justify="center" class="mt-1">
-						<v-col cols="auto" class="text-center">
-							<div class="text-caption">Nothing was found.</div>
-							<v-btn plain text @click="clearSearch">Clear Search</v-btn>
-						</v-col>
-					</v-row>
-				</template>
-			</v-data-iterator>
-			<resume-dialog-skill ref="skillDialog" />
-		</v-container>
-	</client-only>
+									{{ item.icon }}
+								</v-icon>
+								<v-card-title>
+									<h1 class="text-h6 font-weight-black">{{ item.title }}</h1>
+									<v-spacer />
+									<v-badge
+										v-if="item.isPreferred"
+										aria-hidden="true"
+										dot
+										bordered
+										:dark="$colour.isReadableInBlack(item.color)"
+										:light="!$colour.isReadableInBlack(item.color)"
+										color="primary"
+										class="ma-4"
+										:style="{ position: 'absolute', top: 0, right: 0 }"
+									/>
+								</v-card-title>
+								<v-card-subtitle v-if="item.subtitle" tag="p">
+									{{ item.subtitle }}
+								</v-card-subtitle>
+								<v-card-text v-if="item.notes">
+									<ul>
+										<li
+											v-for="(note, index) in item.notes"
+											:key="`${item.title}-note-${index}`"
+										>
+											{{ note }}
+										</li>
+									</ul>
+								</v-card-text>
+								<v-card-actions>
+									<v-btn
+										text
+										plain
+										@click="item.dialogIsShown = !item.dialogIsShown"
+									>
+										{{ $t('$vuetify.close') }}
+									</v-btn>
+									<v-spacer />
+									<v-btn
+										v-if="item.link"
+										text
+										link
+										:href="item.link"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										<v-icon left>$icon.link</v-icon>
+										{{ $t('Learn More') }}
+									</v-btn>
+								</v-card-actions>
+							</v-card>
+						</v-dialog>
+					</v-col>
+				</v-row>
+			</template>
+			<template #no-results>
+				<v-row justify="center" class="mt-1">
+					<v-col cols="auto" class="text-center">
+						<div class="text-caption">Nothing was found.</div>
+						<v-btn plain text @click="clearSearch">Clear Search</v-btn>
+					</v-col>
+				</v-row>
+			</template>
+		</v-data-iterator>
+	</v-container>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Ref, Vue } from 'nuxt-property-decorator'
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
 import type { Skill } from '~/plugins/resume/models'
 
 @Component
@@ -107,9 +189,6 @@ export default class ResumeGridSkillsets extends Vue {
 	@Prop({ required: true })
 	items!: Skill[]
 
-	@Ref()
-	readonly skillDialog!: Vue
-
 	searchItems(items: any[], search: string) {
 		return search === '' || search === null
 			? items
@@ -126,10 +205,6 @@ export default class ResumeGridSkillsets extends Vue {
 
 	clearSearch() {
 		this.search = null
-	}
-
-	openInDialog(skill: Skill) {
-		this.skillDialog.$emit('pass-and-open-dialog', skill)
 	}
 }
 </script>
