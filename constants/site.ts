52f338a706ea78f587f6451603c6bb043391bdad
Armin <armin.monirzadeh@gmail.com>
2022-02-16 08:58:35 +1100

    define site constants

---
+++ constants/site.ts
@@ -0,0 +1,9 @@
+export const BASE_URL = 'https://arminmon.dev/'
+export const SITE_DESCRIPTION = 'Background, resumé, interests, and more.'
+export const SITE_IMAGE = {
+	url: '/images/memoji/wave.png',
+	width: '298',
+	height: '294',
+}
+export const SITE_NAME = "Armin Monirzadeh's portfolio"
+export const SITE_TITLE = 'Portfolio'
